e94bfcb254f1ea63424393ec26c9fd57174706c7
Shaneann01 <shaneann.ahtack@vodafone.com>
2023-11-06 14:42:59 +0000

    testing musicians endpoint

--- index.test.js
+++ index.test.js
@@ -1,22 +1,21 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
-
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+describe("./musicians endpoint", () => {
+  // Write your tests here
+  test("Testing musicians endpoint", async () => {
+    const response = await request(app).get("/musicians");
+    const responseData = JSON.parse(response.text);
+    console.log(responseData);
+    expect(response.statusCode).toBe(200);
+    expect(typeof responseData).toBe("object");
+  });
+});
