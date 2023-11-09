a5dbe371652b5c66ec4a31a683f084e9288ad8de
Shaneann01 <shaneann.ahtack@vodafone.com>
2023-11-09 14:41:41 +0000

    create,update,delete is done

--- src/app.js
+++ src/app.js
@@ -29,4 +29,28 @@ app.get("/bands/:id", async (req, res) => {
   const bands = await Band.findByPk(id);
   res.json(bands);
 });
+
+// Put/Post/Delete
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post("/musicians", async (req, res) => {
+  const musicians = await Musician.create(req.body);
+  res.json(musicians);
+});
+
+app.put("/musicians/:id", async (req, res) => {
+  const updateMusician = await Musician.update(req.body, {
+    where: { id: req.params.id },
+  });
+  res.json(updateMusician);
+});
+
+app.delete("/musicians/:id", async (req, res) => {
+  const deleteMusciain = await Musician.destroy({
+    where: { id: req.params.id },
+  });
+  res.json(deleteMusciain);
+});
 module.exports = app;
